9eadad94819a7dbdfea26a9c25dd9f3bc97aa594
meleciosilva <mpsilva94@gmail.com>
2021-06-01 13:59:03 -0400

    move index to src

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const app = express();
 const port = process.env.PORT || 4000;
 const cors = require("cors");
 
-const decksRouter = require("./src/routers/decks.router");
-const cardsRouter = require("./src/routers/cards.router");
+const decksRouter = require("./routers/decks.router");
+const cardsRouter = require("./routers/cards.router");
 
-app.use(cors({ origin: "http://localhost:3000" }))
+app.use(cors({ origin: "*" }))
 app.use(express.json());
 
 app.use("/decks", decksRouter);
