a29157bfa113ae6ab68186871e1a5a5954c84ae9
meleciosilva <mpsilva94@gmail.com>
2021-06-01 13:25:52 -0400

    add router

--- src/routers/cards.router.js
+++ src/routers/cards.router.js
@@ -1,6 +1,15 @@
-const router = require("express").Router();
+const router = require("express").Router({ mergeParams: true });
 const controller = require("./../controllers/cards.controller");
 
+router
+  .route("/:cardId")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.delete)
 
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
 
 module.exports = router;
