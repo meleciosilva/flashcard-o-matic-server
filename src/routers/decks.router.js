f9691afd805b11622303d2480e80457bb9d75f28
meleciosilva <mpsilva94@gmail.com>
2021-06-17 14:41:41 -0400

    add methodNotAllowed

--- src/routers/decks.router.js
+++ src/routers/decks.router.js
@@ -1,23 +1,27 @@
 const router = require("express").Router();
 const controller = require("./../controllers/decks.controller");
 const cardsRouter = require("./../routers/cards.router");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.use("/:deckId/cards", controller.deckExists, cardsRouter);
 
 router
   .route("/:deckId/cards")
   .get(controller.read)
+  .all(methodNotAllowed);
 
 router
   .route("/:deckId")
   .get(controller.read)
   .put(controller.update)
   .delete(controller.delete)
+  .all(methodNotAllowed);
 
 router
   .route("/")
   .get(controller.list)
   .post(controller.create)
+  .all(methodNotAllowed);
 
 
 module.exports = router;
