71176e3570935539de3faeddab5f01ee57457943
meleciosilva <mpsilva94@gmail.com>
2021-06-17 14:39:54 -0400

    add server.js

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const { PORT = 5000 } = process.env;
+const app = require("./app");
+const knex = require("./db/connection");
+
+knex.migrate
+  .latest()
+  .then((migrations) => {
+    console.log("migrations", migrations);
+    app.listen(PORT, listener);
+  })
+  .catch((error) => {
+    console.error(error);
+    knex.destroy();
+  });
+
+const listener = () => console.log(`Server up and running on port ${PORT}!`);
